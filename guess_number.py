ff7cedbe6ad680d751fd8d9e22d67974c51ad37f
McNeil McCarley <mcneilmccarley@yahoo.com>
2024-03-10 22:31:25 -0500

    Refactored to give user limited guesses

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,6 @@
 number = 10
+max_guesses = 3
+guess_count = 0
 
 while True:
     print("I'm thinking of a number...")
@@ -14,8 +16,12 @@ while True:
        print("Enter a valid number or 'q' to quit.")
        continue
 
+    guess_count += 1
+
     if guess_input == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif guess_count < max_guesses:
+        print(f"That's incorrect. You have {max_guesses - guess_count} {'guesses' if max_guesses - guess_count > 1 else 'guess'} left. Try again.")
     else:
-        print(f"Sorry! The number was {number}.")
+        print(f"Sorry! You ran out of guesses. The number was {number}.")
